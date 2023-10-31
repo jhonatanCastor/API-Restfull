4133981ddd53923f0174151cc515fe9b7e3b0147
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2023-10-30 21:57:17 -0300

    routers of class the service product

---
+++ src/modules/products/routes/product.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { ProductController } from "@modules/controllers/ProductsControllers";
+import { celebrate, Joi, Segments } from "celebrate";
+
+const productRouter = Router();
+const productController = new ProductController();
+
+productRouter.get('/', productController.index);
+
+productRouter.get('/:uid', celebrate({
+  [Segments.PARAMS]: {
+    uid: Joi.string().uuid()
+  },
+}), productController.show);
+
+productRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required()
+    },
+  }),
+  productController.create);
+
+productRouter.put('/:uid', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    price: Joi.number().precision(2).required(),
+    quantity: Joi.number().required()
+  },
+  [Segments.PARAMS]: {
+    uid: Joi.string().uuid()
+  },
+}), productController.update);
+
+productRouter.delete('/:uid', celebrate({
+  [Segments.PARAMS]: {
+    uid: Joi.string().uuid()
+  },
+}), productController.delete);
+
+export default productRouter;
