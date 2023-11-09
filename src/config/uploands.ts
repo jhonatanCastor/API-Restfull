681ec7dee871a0d7de34baa5cb6f1450cfd6996c
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2023-11-09 11:36:24 -0300

    service routers

---
+++ src/config/uploands.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import multer from 'multer';
+import crypto from 'crypto';
+
+const uploadsFolder = path.resolve(__dirname, '..', '..', 'uploads')
+
+export default {
+  directory: uploadsFolder,
+  storage: multer.diskStorage({
+    destination: uploadsFolder,
+    filename(request, file, callback) {
+      const fileHast = crypto.randomBytes(10).toString('hex')
+
+      const fileName = `${fileHast}-${file.originalname}`
+
+      callback(null, fileName)
+
+    }
+  })
+} 
--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,14 @@
+import userAvatar from "@modules/Avatar/router/avatar.routes";
+import sessionRouter from "@modules/Session/routers/session.routes";
 import productRouter from "@modules/products/routes/product.routes";
+import userRouter from "@modules/user/routes/user.routes";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/user', userRouter);
+routes.use('/session', sessionRouter);
+routes.use('/avatar', userAvatar)
 
 export default routes;
