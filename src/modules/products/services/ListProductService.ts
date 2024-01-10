289e75c7c47ff4dda3977294d547134fe37409b8
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-10 20:03:51 -0300

    feature: add link to entity products

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,11 +1,13 @@
 import { ProductRepository } from "@modules/products/repositories/ProductRepository";
 import { Product } from "@prisma/client";
+import { addLinksToEntityList } from "@utils/hateoasUtils";
 export class ListProductService {
+  private domain = 'products'
   public async execute(): Promise<Product[]> {
     const productsRepository = new ProductRepository;
 
     const products = await productsRepository.find();
 
-    return products;
+    return addLinksToEntityList(products as Product[], this.domain);
   }
 }
