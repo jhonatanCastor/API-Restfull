b4a8ce5f2925d889906b15a71e9f7f3d4aad9ac8
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-11 22:46:06 -0300

    hotfix: changed name the route

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -7,7 +7,7 @@ import { Router } from "express";
 const routes = Router();
 
 routes.use('/products', productRouter);
-routes.use('/user', userRouter);
+routes.use('/users', userRouter);
 routes.use('/session', sessionRouter);
 routes.use('/avatar', userAvatar)
 
