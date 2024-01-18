0086bd3a949a1edb7efd898a980cdc8972d7aae4
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-18 12:01:33 -0300

    feature: create controller ForgotPass

---
+++ src/modules/user/tokes/controller/ForgotPasswordController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { SendForgotPasswordService } from "@modules/user/tokes/services/SendForgotPasswordService";
+
+export class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response>{
+      const { email } = request.body;
+
+      const sendForgotPassword = new SendForgotPasswordService();
+      
+      await sendForgotPassword.execute(email);
+      return response.status(204).json();
+  }
+}
