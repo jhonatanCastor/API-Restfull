2f847ae6eb8c96d6d6f5a329d685a0e698f2ab69
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-19 11:35:34 -0300

    feature: create UserTokensRespository

--- src/modules/user/tokes/repository/UserTokensRepository.ts
+++ src/modules/user/tokes/repository/UserTokensRepository.ts
@@ -1,13 +1,7 @@
 import AppError from "@/shared/errors/AppError";
 import prisma from "@/utils/PrismaClient";
-import { Prisma, User_Tokens } from "@prisma/client";
+import { User_Tokens } from "@prisma/client";
 
-interface IRequest {
-  uid: string;
-  token: string;
-  expiresAt: Date | null;
-  user_uid: string;
-}
 export class UserTokes {
 
   public async generate(user_uid: string): Promise<User_Tokens> {
@@ -17,21 +11,6 @@ export class UserTokes {
     });
 
     return userToken;
-  }
-
-  async save(token: Prisma.User_TokensUpdateInput): Promise<User_Tokens | undefined> {
-    const updatedUser = await prisma.user_Tokens.update({
-      where: {
-        uid: (token.uid as string)
-      },
-      data: token,
-    })
-
-    if (!updatedUser) {
-      throw new AppError("Update failed")
-    }
-
-    return updatedUser;
   }
 
   async findByToken(token: string): Promise<User_Tokens | undefined> {
