cae53f08019af1c3661558bf05609695ca070571
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-19 11:36:22 -0300

    feature: add ResetPasswordController to routes

--- src/modules/user/tokes/routes/password.routes.ts
+++ src/modules/user/tokes/routes/password.routes.ts
@@ -2,15 +2,26 @@ import { celebrate, Joi, Segments } from "celebrate";
 import { Router } from "express";
 import isAuthenticated from "@modules/session/middlewares/isAuthenticated";
 import { ForgotPasswordController } from "@modules/user/tokes/controller/ForgotPasswordController";
+import ResetPasswordController from "../controller/ResetPasswordController";
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
-passwordRouter.post('/forgot', 
+passwordRouter.post('/forgot',
   celebrate({
     [Segments.BODY]: {
       email: Joi.string().email().required(),
     }
   }), forgotPasswordController.create);
+
+  passwordRouter.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    }
+  }), resetPasswordController.create);
 
 export default passwordRouter; 
