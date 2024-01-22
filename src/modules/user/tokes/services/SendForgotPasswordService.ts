42fb66152a9da3c9f635f52ef1009f3dfbc51bd0
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-22 13:57:39 -0300

    feature: teste set await EtherealMail

--- src/modules/user/tokes/services/SendForgotPasswordService.ts
+++ src/modules/user/tokes/services/SendForgotPasswordService.ts
@@ -2,23 +2,28 @@ import AppError from "@shared/errors/AppError";
 import { UserRepository } from "../../repository/UserRepository";
 import { UserTokes } from "../repository/UserTokensRepository";
 import Logger from "@/utils/wisntonLogger";
-
-
+import EtherealMail from "@/config/mail/EthereImail";
 export class SendForgotPasswordService {
-  
+
   public async execute(email: string): Promise<void> {
     const usersRepository = new UserRepository();
     const userTokenRepository = new UserTokes();
 
     const user = await usersRepository.findByEmail(email);
     Logger.info(user);
-    
+
 
     if (!user) {
       throw new AppError('User does not exists.', 404)
     }
 
     const token = await userTokenRepository.generate(user.uid as string);
-    Logger.info(token)
+
+    console.log(token);
+
+
+    // await EtherealMail.sendMail({
+    //   subject, to, from, template
+    // })
   }
 }
