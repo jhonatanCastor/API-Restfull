144b41096cda35aa09bf5649e4c25051a11e7233
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-01-29 11:26:33 -0300

    feature: create serviceDeleteService

---
+++ src/modules/customers/services/DeleteCustomersService.ts
@@ -0,0 +1,16 @@
+import AppError from "@/shared/errors/AppError";
+import { CustomersRepository } from "@/modules/customers/repositories/CustomersRepository";
+
+export class DeleteCustomersService {
+   public async execute(uid: string) {
+    const customersRepository = new CustomersRepository();
+
+    const customersExist = await  customersRepository.findByUid(uid);
+
+    if(!customersExist){
+      throw new AppError("Customer does not exist!");
+    }
+    
+    await customersRepository.delete(uid);
+   }
+}
