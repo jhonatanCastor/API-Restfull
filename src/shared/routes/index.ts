c4d684f238d918f6baab306c56642e5e0ccbcbff
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-02-01 11:25:25 -0300

    feature: add routes to index

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -6,6 +6,7 @@ import { Router } from "express";
 import passwordRouter from "@/modules/user/tokes/routes/password.routes";
 import profileRouter from "@/modules/user/routes/profile.routes";
 import customerRouter from "@/modules/customers/routes/customers.routes";
+import ordersRouter from "@/modules/orders/routes/ orders.routes";
 
 const routes = Router();
 
@@ -16,5 +17,6 @@ routes.use('/avatar', userAvatar)
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
 routes.use('/customer', customerRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
